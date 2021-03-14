47c1dfd4dda7210b44bf3b3b0d12bf2ec9c70ad9
Upasana Ghosh <upasana.ghosh@stonewain.com>
2021-03-14 19:41:04 +0530

    Display all negative number receieved and throw error

--- src/main/java/testbaseddevlopment/StringCalculator.java
+++ src/main/java/testbaseddevlopment/StringCalculator.java
@@ -1,5 +1,9 @@
 package testbaseddevlopment;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class StringCalculator {
 
 	/**
@@ -7,6 +11,7 @@ public class StringCalculator {
 	 * The method can take 0, 1 or 2 numbers, and will return their sum (for an
 	 * empty string it will return 0) for example “” == 0 , “1” == 1 , “1,2” ==
 	 * 3
+	 *
 	 * @throws NegativeNumberException
 	 */
 	public int add(String numbers) throws NegativeNumberException {
@@ -14,18 +19,22 @@ public class StringCalculator {
 		if (numbers == null || numbers.trim().isEmpty())
 			return 0;
 
-		//remove '\n' and '//'
-		//split on ',' or ';'
+		// remove '\n' and '//'
+		// split on ',' or ';'
 		String numberList[] = numbers.replaceAll("[\n//]+", ",").split(",|;");
 
 		int res = 0;
+		List<String> negativeNum = new ArrayList<>();
 		for (String num : numberList) {
 
 			if (num.contains("-")) {
-				throw new NegativeNumberException("negatives not allowed: " + num);
-			}
-			if (!num.trim().isEmpty())
+				negativeNum.add(num);
+			} else if (!num.trim().isEmpty())
 				res += Integer.parseInt(num);
+		}
+
+		if (!negativeNum.isEmpty()) {
+			throw new NegativeNumberException("negatives not allowed" + negativeNum);
 		}
 		return res;
 	}
